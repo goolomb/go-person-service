6703a2c6fead44c93f92e15a0d013a9f18888460
goolomb <martin.golomb@seznam.cz>
2026-05-05 16:35:13 +0200

    Decode save request JSON

--- internal/httpapi/handler.go
+++ internal/httpapi/handler.go
@@ -12,7 +12,27 @@ func HealthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func SaveHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	var request SavePersonRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(ErrorResponse{
+			Error:   "invalid_json",
+			Message: "Request body must be a valid JSON object.",
+		})
+		return
+	}
+
+	response := PersonResponse{
+		ExternalID:  request.ExternalID,
+		Name:        request.Name,
+		Email:       request.Email,
+		DateOfBirth: request.DateOfBirth,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(response)
 }
 
 func GetByIDHandler(w http.ResponseWriter, r *http.Request) {
